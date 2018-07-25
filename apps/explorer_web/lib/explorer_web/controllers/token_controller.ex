079513beca164b89eec8a946c2cb0b33327af398
Amanda Sposito <amandinha.sposito@gmail.com>
2018-07-25 14:29:33 -0300

    Add token details page querys

--- apps/explorer_web/lib/explorer_web/controllers/token_controller.ex
+++ apps/explorer_web/lib/explorer_web/controllers/token_controller.ex
@@ -1,10 +1,34 @@
 defmodule ExplorerWeb.TokenController do
   use ExplorerWeb, :controller
 
-  def show(conn, %{"id" => id, "locale" => locale}) do
-    render(
-      conn,
-      "show.html"
-    )
+  alias Explorer.Chain
+  alias Explorer.Chain.{Token, TokenTransfer}
+
+  import ExplorerWeb.Chain, only: [split_list_by_page: 1, paging_options: 1, next_page_params: 3]
+
+  def show(conn, %{"id" => address_hash_string} = params) do
+    with {:ok, address_hash} <- Chain.string_to_address_hash(address_hash_string) do
+      token = Token.token_from_address(address_hash)
+
+      token_transfers_plus_one = TokenTransfer.fetch_token_transfers(address_hash, paging_options(params))
+
+      {token_transfers, next_page} = split_list_by_page(token_transfers_plus_one)
+
+      render(
+        conn,
+        "show.html",
+        transfers: token_transfers,
+        token: token,
+        total_token_transfers: TokenTransfer.count_token_transfers(address_hash),
+        total_address_in_token_transfers: TokenTransfer.count_addresses_in_transfers(address_hash),
+        next_page_params: next_page_params(next_page, token_transfers, params)
+      )
+    else
+      :error ->
+        not_found(conn)
+
+      {:error, :not_found} ->
+        not_found(conn)
+    end
   end
 end
